eb16735bad10fed7584041e32f67faedbf74af24
Benjamín Morozov <benjaminmorozov@gmail.com>
2021-07-24 02:47:16 +0200

    Update to blockthespot installer and uninstaller

---
+++ uninstall.bat
@@ -0,0 +1,23 @@
+@echo off
+echo Removing Patch
+if exist "%APPDATA%\Spotify\chrome_elf.dll.bak" (
+    del /s /q "%APPDATA%\Spotify\chrome_elf.dll" > NUL 2>&1
+    move "%APPDATA%\Spotify\chrome_elf.dll.bak" "%APPDATA%\Spotify\chrome_elf.dll" > NUL 2>&1
+) else (
+    echo done
+)
+
+if exist "%APPDATA%\Spotify\Apps\zlink.spa.bak" (
+    del /s /q "%APPDATA%\Spotify\Apps\zlink.spa" > NUL 2>&1
+    move "%APPDATA%\Spotify\Apps\zlink.spa.bak" "%APPDATA%\Spotify\Apps\zlink.spa" > NUL 2>&1
+) else (
+    echo done
+)
+
+if exist "%APPDATA%\Spotify\Apps\xpui.spa.bak" (
+    del /s /q "%APPDATA%\Spotify\Apps\xpui.spa" > NUL 2>&1
+    move "%APPDATA%\Spotify\Apps\xpui.spa.bak" "%APPDATA%\Spotify\Apps\xpui.spa" > NUL 2>&1
+) else (
+    echo done
+)
+pause
